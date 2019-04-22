3906aa80ba4cd2583033b5f742bc141daca58dba
Amita <amita.parikh@gmail.com>
2019-04-22 16:38:36 +0000

    Added in rspec testing files

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,17 +13,17 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # /products/:product_id/reviews/:id
-  def destroy
-    @product = Product.find params[:product_id]
-    @review = Review.find params[:id]
-
-    if @review.destroy
-      redirect_to product_path(@product.id.reviews)
-    else
-      render 'products/show'
-    end
-  end
+  # # /products/:product_id/reviews/:id
+  # def destroy
+  #   @product = Product.find params[:product_id]
+  #   @review = Review.find params[:id]
+  # 
+  #   if @review.destroy
+  #     redirect_to product_path(@product.id.reviews)
+  #   else
+  #     render 'products/show'
+  #   end
+  # end
 
   private
 
