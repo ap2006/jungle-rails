2640b5ff8629d8291334e38813c95b402e1bae8e
Amita <amita.parikh@gmail.com>
2019-04-22 18:23:39 +0000

    Added in product tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+      # validation tests/examples here
+      subject do
+      @category = Category.create(name: 'Fruit')
+      @category.products.new(
+        name: 'Pineapple',
+        price: 55,
+        quantity: 30
+      )
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+  it 'is not valid without a price' do
+    subject.price_cents = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a quantity' do
+    subject.quantity = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a category' do
+    subject.category = nil
+    expect(subject).to_not be_valid
+    end
+  end
+end
